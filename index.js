d7097ed521f0f907619a68dc8c09a0e147c8e16f
Brenda <brenda4158@gmail.com>
2022-10-12 17:07:48 -0300

    Instalando nodemon

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
-
-
 const app = express();
+
 app.get('/', (request, response) => {
     return response.json({
         message: "Olá Mundo!"
     })
 });
 
+app.get('/pessoas/eu', (req, res) => {
+    return res.json({
+        Nome: 'Brenda',
+        Idade: 21,
+        EstadoCivil: 'União Estável',
+    });
+});
+
+
 app.listen(8000, () => {
-    console.log('servidor começou')
+    console.log('servidor iniciado')
 })
