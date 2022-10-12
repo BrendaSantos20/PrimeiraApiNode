0f035eb7b94058ca3cead6069d500c807c616d2f
Brenda <brenda4158@gmail.com>
2022-10-12 18:09:11 -0300

    Adicionando objetos em Arrays

--- index.js
+++ index.js
@@ -1,19 +1,46 @@
+
 const express = require('express');
 const app = express();
+app.use(express.json())
 
-app.get('/', (request, response) => {
-    return response.json({
-        message: "Olá Mundo!"
-    })
+const pessoas = [
+    {
+        nome: 'Binho',
+        idade: 24,
+        elo: 'Imortal'
+    }
+];
+app.get('/pessoas', (request, response) => {
+    return response.json(pessoas)
 });
 
-app.get('/pessoas/eu', (req, res) => {
-    return res.json({
-        Nome: 'Brenda',
-        Idade: 21,
-        EstadoCivil: 'União Estável',
+app.post('/pessoas', (req, res) => {
+    const novaPessoa = {
+        nome: req.body.nome,
+        idade: req.body.idade,
+        elo: req.body.elo
+    }
+    pessoas.push(novaPessoa)
+    return res.json(novaPessoa)
+
+})
+
+app.get('/pessoas/:nome', (req, res) => {
+    const nome = req.params.nome;
+    const pessoa = pessoas.find((p) => {
+        return p.nome === nome
     });
-});
+    return res.json(pessoa)
+})
+
+// Criar uma API de soma. Vai receber dois parametros de rota, vai retornar a soma dos dois
+// app.get('/pessoas/eu', (req, res) => {
+//     return res.json({
+//         Nome: 'Brenda',
+//         Idade: 21,
+//         EstadoCivil: 'União Estável',
+//     });
+// });
 
 
 app.listen(8000, () => {
